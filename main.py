081cd3799ee12852d33e691124522b6c93bf10fa
Davide Esposito Pelella <122233783+DavideEspositoPelella@users.noreply.github.com>
2023-12-12 15:15:32 +0100

    evaluate working

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ try:
 except OSError:
     pass
 
-#TODO the evaluation function must be updated to work with the new environment
+
 def evaluate(algorithm: str='ddqn',
              episodes: int=20000) -> None:
 
@@ -33,39 +33,19 @@ def evaluate(algorithm: str='ddqn',
         - algorithm (str): The algorithm to use to train the agent. Available options are 'ddqn', 'ddqn_per', 'a3c', 'dueling_ddqn', and 'sarsa'. Defaults to 'ddqn'.
         - episodes (int): The number of episodes to train for. Defaults to 20000.
     """
-    env_ID = "SuperMarioBros-1-1-v0"
-    if gym.__version__ < '0.26':
-        env = gym_super_mario_bros.make(env_ID,
-                                        new_step_api=True)
-
-    else:
-        env = gym_super_mario_bros.make(env_ID, 
-                                        render_mode='human', 
-                                        apply_api_compatibility=True)
-    if algorithm == 'ddqn':
-        agent = DDQN(episodes=episodes, 
-                     prioritized=False)
-    elif algorithm == 'ddqn_per':
-        agent = DDQN(episodes=episodes, 
-                     prioritized=True)
-    else:
-        raise ValueError("Invalid algorithm selected")
+    with torch.no_grad():
+        if algorithm == 'ddqn':
+            agent = DDQN(episodes=episodes, 
+                        prioritized=False)
+        elif algorithm == 'ddqn_per':
+            agent = DDQN(episodes=episodes, 
+                        prioritized=True)
+        else:
+            raise ValueError("Invalid algorithm selected")
+        agent.load()
+        agent.evaluate(episodes)
     
-    # agent.load()
-    # rewards = []
-    # for episode in range(episodes):
-    #     total_reward = 0
-    #     done = False
-    #     state, _ = env.reset()
-    #     while not done:
-    #         action = agent.act(state)
-    #         state, reward, terminated, truncated, info = env.step(action)
-    #         done = terminated or truncated
-    #         total_reward += reward
 
-    #     rewards.append(total_reward)
-
-    # print('Mean Reward:', np.mean(rewards))
 
 def train(algorithm: str='ddqn', 
           episodes: int=20000) -> None:
